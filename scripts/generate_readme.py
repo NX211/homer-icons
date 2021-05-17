c5ad00e5b8153f8fee4dd6545784da745119c2ef
Gaëtan Caillaut <gcaillaut@protonmail.com>
2021-05-17 23:37:51 +0200

    script generating the readme file based on available icons

---
+++ scripts/generate_readme.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+def generate_img_tag(file):
+    return f'<img src="{file.name}" alt="{file.stem}" width="50">'
+
+if __name__ == "__main__":
+    imgs = sorted(Path(".").glob("*.png"))
+    img_tags = [generate_img_tag(x) for x in imgs]
+
+    with open("README.md", "wt", encoding="UTF-8") as f:
+        f.write("# Homer Icons\n\n")
+        f.write("[Homer Dashboard](https://github.com/bastienwirtz/homer)\n\n")
+        f.write(" ".join(img_tags))
+        f.write("\n")
